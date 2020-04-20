0d9fbd637946b085ee48c25de18b783754293569
Tim van Dam <timvandamcs@gmail.com>
2020-04-20 17:13:11 +0200

    removed more js files

--- src/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.server = {
-    port: parseInt(process.env.PORT || '25565')
-};
-exports.logger = {
-    logLevel: process.env.NODE_ENV === 'production' ? 'info' : 'verbose'
-};

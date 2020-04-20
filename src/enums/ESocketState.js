5f0483690ce53c11894036bb0086ac05b776d677
Tim van Dam <timvandamcs@gmail.com>
2020-04-20 17:09:07 +0200

    removed js files

--- src/enums/ESocketState.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ESocketState;
-(function (ESocketState) {
-    ESocketState[ESocketState["HANDSHAKING"] = 0] = "HANDSHAKING";
-    ESocketState[ESocketState["STATUS"] = 1] = "STATUS";
-    ESocketState[ESocketState["LOGIN"] = 2] = "LOGIN";
-    ESocketState[ESocketState["PLAY"] = 3] = "PLAY";
-})(ESocketState = exports.ESocketState || (exports.ESocketState = {}));

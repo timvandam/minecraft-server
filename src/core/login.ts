24b387333571ca557399bac1da67ea1199687d7e
Tim van Dam <timvandamcs@gmail.com>
2020-04-21 23:40:41 +0200

    small login stuff

--- src/core/login.ts
+++ src/core/login.ts
@@ -1,4 +1,8 @@
 import { EventEmitter } from 'events'
+import MinecraftClient from '../MinecraftClient'
 
 export default function login (user: EventEmitter) {
+  user.on('loginStart', (client: MinecraftClient) => {
+  //  TODO: Login routine
+  })
 }

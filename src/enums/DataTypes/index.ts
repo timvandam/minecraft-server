14c1b5e4e583da460f9e9ff4dc86a7008b5d9cdd
Tim van Dam <timvandamcs@gmail.com>
2020-04-27 22:58:11 +0200

    made NBT types consistent with MC types, also added convenience methods

--- src/enums/DataTypes/index.ts
+++ src/enums/DataTypes/index.ts
@@ -1,41 +1,87 @@
 // These datatypes are for Java edition Minecraft - so big endian stuff
 
-// TODO: Format these the same way as Minecraft datatypes for consistency sake
-
-interface DataType {
-  readonly value: number;
-  readonly buffer: Buffer;
-}
+import { DataType } from '../../DataTypes/DataType'
 
 // A single signed byte
-export class Byte implements DataType {
-  public readonly value: number
-  public readonly buffer: Buffer
-
-  constructor (value: number) {
-    this.value = value
-    this.buffer = Buffer.allocUnsafe(1)
-    this.buffer.writeInt8(value)
+export class Byte extends DataType<number> {
+  protected read (data: Buffer): number {
+    return data.readInt8()
   }
 
-  static read (buffer: Buffer) {
-    return new Byte(buffer.readInt8())
+  protected write (value: number): Buffer {
+    const buf = Buffer.allocUnsafe(1)
+    buf.writeInt8(value)
+    return buf
   }
 }
+export const byte = (value: number) => new Byte({ value })
 
 // A 2-byte signed short
-export class Short implements DataType {
-  public readonly value: number
-  public readonly buffer: Buffer
-
-  constructor (value: number) {
-    this.value = value
-    this.buffer = Buffer.allocUnsafe(2)
-    this.buffer.writeInt16BE(value)
+export class Short extends DataType<number> {
+  protected read (data: Buffer): number {
+    return data.readInt16BE()
   }
 
-  static read (buffer: Buffer) {
-    return new Short(buffer.readInt16BE())
+  protected write (value: number): Buffer {
+    const buf = Buffer.allocUnsafe(2)
+    buf.writeInt16BE(value)
+    return buf
   }
-
 }
+export const short = (value: number) => new Short({ value })
+
+// A 4 byte int
+export class Int extends DataType<number> {
+  protected read (data: Buffer): number {
+    return data.readInt32BE()
+  }
+
+  protected write (value: number): Buffer {
+    const buf = Buffer.allocUnsafe(4)
+    buf.writeInt32BE(value)
+    return buf
+  }
+}
+export const int = (value: number) => new Int({ value })
+
+// A 8 bit long
+export class Long extends DataType<bigint> {
+  protected read (data: Buffer): bigint {
+    return data.readBigInt64BE()
+  }
+
+  protected write (value: bigint): Buffer {
+    const buf = Buffer.allocUnsafe(8)
+    buf.writeBigInt64BE(value)
+    return buf
+  }
+}
+export const long = (value: bigint) => new Long({ value })
+
+// A 4 byte float
+export class Float extends DataType<number> {
+  protected read (data: Buffer): number {
+    return data.readFloatBE()
+  }
+
+  protected write (value: number): Buffer {
+    const buf = Buffer.allocUnsafe(4)
+    buf.writeFloatBE(value)
+    return buf
+  }
+}
+export const float = (value: number) => new Float({ value })
+
+// A 8 byte double
+export class Double extends DataType<number> {
+  protected read (data: Buffer): number {
+    return data.readDoubleBE()
+  }
+
+  protected write (value: number): Buffer {
+    const buf = Buffer.allocUnsafe(8)
+    buf.writeDoubleBE(value)
+    return buf
+  }
+}
+export const double = (value: number) => new Double({ value })
